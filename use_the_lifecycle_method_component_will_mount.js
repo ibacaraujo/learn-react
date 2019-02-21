e622327f3fed75ccc6b251276b3991ae4a213843
Iago Breno <ibacaraujo@gmail.com>
2019-02-21 17:59:11 -0300

    Add use_the_lifecycle_method_component_will_mount.js

---
+++ use_the_lifecycle_method_component_will_mount.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // change code below this line
+    console.log("Component is being mounted to the DOM.")
+    // change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
